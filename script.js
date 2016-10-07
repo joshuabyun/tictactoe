a3fce254c06aad2e0f118bca07e0924465403073
Joshua Byun <byunsung89@gmail.com>
2016-10-06 17:45:38 -0700

    re-structured game winning condition and winning handler

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 $( document ).ready(function(){
     applyClickHandler();
 });
-var scoreBoard = [[0,0,0,0,0],
-                   [0,0,0,0,0],
-                   [0,0,0,0,0],
-                   [0,0,0,0,0],
-                   [0,0,0,0,0]];
+var scoreBoard = [[0,0,0],
+                   [0,0,0],
+                   [0,0,0]];
 var counter = 0;
 var applyClickHandler = function(){
   $('.tictactoeCell').click(function(){
@@ -17,7 +15,7 @@ var applyClickHandler = function(){
       changeScoreBoard($(this).parent().index(),$(this).index(),player);
       changeCellColor(this,player);
       differentiateClickedCell(this);
-      initScoreReaders();
+      initScoreSpecifiedScoreReaders($(this).parent().index(),$(this).index(),player);
       counter++;
   });
 };
@@ -52,56 +50,78 @@ var changeCellColor = function(clickedCell,player){
             break;
     }
 };
-var initScoreReaders = function(){
-    if(scoreReader(winHandler,"horizontal")){
-        return;
-    }
-    if(scoreReader(winHandler,"vertical")){
-        return true;
-    }
-    diagonalScoreReader(winHandler);
-};
-var scoreReader = function(winHandler,checkType){
-    for(var i = 0; i < scoreBoard.length; i++){
-      var sum = 0;
-      for(var j = 0; j < scoreBoard.length; j++){
-          switch (checkType){
-              case "horizontal":
-                  sum += scoreBoard[i][j];
-                  break;
-              case "vertical":
-                  sum += scoreBoard[j][i];
-                  break;
-          }
-          if(sum == scoreBoard.length || sum == -1*scoreBoard.length ){
-              winHandler(sum,checkType);
-              return true;
-          }
-      }
-  }
-};
-var diagonalScoreReader = function(winHandler){
-    var sum1 = 0;
-    var sum2 = 0;
-    var i;
-    var col = scoreBoard.length-1;
-    for(i = 0;i <scoreBoard.length; i++){
-        sum1 += scoreBoard[i][i];
-        sum2 += scoreBoard[i][col-i];
-        if(sum1 == scoreBoard.length || sum1 == -1*scoreBoard.length){
-            winHandler(sum1,"top left to bottom right");
-            return true;
-        }else if(sum2 == -1*scoreBoard.length || sum2 == scoreBoard.length){
-            winHandler(sum2,"top right to bottom left");
-            return true;
+var initScoreSpecifiedScoreReaders = function(rowIndex,colIndex){
+    var clickedPos = [rowIndex,colIndex];
+    var centerPos = [Math.floor(scoreBoard.length/2)-1,Math.floor(scoreBoard.length/2)-1];
+    var rowSum;
+    var colSum;
+    var diagonal1Sum;
+    var diagonal2Sum;
+    switch(clickedPos){
+        case [0,0]:
+            rowSum = checkHorizontalRow(clickedPos[1]);
+            colSum = checkVerticalCol(clickedPos[0]);
+            diagonal1Sum = diagonalTopLeftBtmRht();
+            break;
+        case [0,scoreBoard.length-1]:
+            rowSum = checkHorizontalRow(clickedPos[1]);
+            colSum = checkVerticalCol(clickedPos[0]);
+            diagonal2Sum =diagonalTopRhtBtmLeft();
+            break;
+        case [scoreBoard.length-1,0]:
+            rowSum = checkHorizontalRow(clickedPos[1]);
+            colSum = checkVerticalCol(clickedPos[0]);
+            diagonal2Sum = diagonalTopRhtBtmLeft();
+            break;
+        case [scoreBoard.length-1,scoreBoard.length-1]:
+            rowSum = checkHorizontalRow(clickedPos[1]);
+            colSum = checkVerticalCol(clickedPos[0]);
+            diagonal1Sum = diagonalTopLeftBtmRht();
+            break;
+        case centerPos:
+            rowSum = checkHorizontalRow(clickedPos[1]);
+            colSum = checkVerticalCol(clickedPos[0]);
+            diagonal1Sum = diagonalTopLeftBtmRht();
+            diagonal2Sum = diagonalTopRhtBtmLeft();
+            break;
+        default :
+            rowSum = checkHorizontalRow(clickedPos[1]);
+            colSum = checkVerticalCol(clickedPos[0]);
         }
-    }
+        checkForWinner(rowSum,colSum,diagonal1Sum,diagonal2Sum);
 };
+var checkForWinner = function(rowSum,colSum,diagonal1Sum,diagonal2Sum){
+    
+};
+var diagonalTopLeftBtmRht = function(){
+    var sum = 0;
+    for(var i = 0;i <scoreBoard.length; i++){
+        sum += scoreBoard[i][i];
+    }
+    return sum;
+};
+var diagonalTopRhtBtmLeft = function(){
+    var sum = 0;
+    var col = scoreBoard.length-1;
+    for(var i = 0;i <scoreBoard.length; i++){
+        sum += scoreBoard[i][col-i];
+    }
+    return sum;
+};
+var checkHorizontalRow = function(rowIndex){
+    var sum =0;
+    scoreBoard[rowIndex].forEach(function(currentValue){
+        sum +=currentValue;
+    });
+    return sum;
+};
+var checkVerticalCol = function(colIndex){
+    var sum = 0;
+    scoreBoard.forEach(function(currentValue,index){
+        sum += scoreBoard[index][colIndex]
+    });
+    return sum;
+};
+var aiFindBestCellPos = function(){
 
-var winHandler = function(winnerScore, winningMethod){
-  if(winnerScore > 0){
-      console.log('gold won by '+ winningMethod);
-  }else if(winnerScore < 0){
-      console.log('blue won by '+ winningMethod);
-  }
 };
